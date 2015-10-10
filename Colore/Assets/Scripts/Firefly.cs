ec64cdee8607ee07417669b70e5f090d623bd9f3
Tim Graupmann <tgraupmann@gmail.com>
2015-10-10 16:45:34 -0700

    Added color changes when clicking

--- Colore/Assets/Scripts/Firefly.cs
+++ Colore/Assets/Scripts/Firefly.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Firefly : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
+    /// <summary>
+    /// Timer for button presses
+    /// </summary>
+    DateTime _mTimer = DateTime.MinValue;
+
+    void OnEnable()
+    {
+        Corale.Colore.Core.Mousepad.Instance.Clear();
+    }
+
+    void OnDisable()
+    {
+        Corale.Colore.Core.Mousepad.Instance.Clear();
+    }
+
+    void Update()
     {
         if (null == Corale.Colore.Core.Mousepad.Instance)
         {
@@ -11,6 +26,23 @@ public class Firefly : MonoBehaviour
             return;
         }
 
-        Corale.Colore.Core.Mousepad.Instance.SetStatic(Corale.Colore.Core.Color.Blue);
+        if (Input.GetMouseButton(0))
+        {
+            if (_mTimer == DateTime.MinValue)
+            {
+                Corale.Colore.Core.Mousepad.Instance.Clear();
+                Corale.Colore.Core.Mousepad.Instance.SetStatic(Corale.Colore.Core.Color.Red);
+            }
+            _mTimer = DateTime.Now + TimeSpan.FromMilliseconds(500);
+        }
+        else if (_mTimer > DateTime.Now)
+        {
+            Corale.Colore.Core.Mousepad.Instance.SetStatic(Corale.Colore.Core.Color.Black);
+        }
+        else if (_mTimer != DateTime.MinValue)
+        {
+            _mTimer = DateTime.MinValue;
+            Corale.Colore.Core.Mousepad.Instance.Clear();
+        }
     }
 }
