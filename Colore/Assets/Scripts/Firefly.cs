06238c462ab0c1b0ba6ef0acea2d6559ef51c659
Tim Graupmann <tgraupmann@gmail.com>
2015-10-10 15:35:06 -0700

    Experimenting with Chroma SDK

---
+++ Colore/Assets/Scripts/Firefly.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Firefly : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        if (null == Corale.Colore.Core.Mousepad.Instance)
+        {
+            Debug.LogError("MousePad instance is null!");
+            return;
+        }
+
+        Corale.Colore.Core.Mousepad.Instance.SetStatic(Corale.Colore.Core.Color.Blue);
+    }
+}
